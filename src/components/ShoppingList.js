71527a461ffef13dfbafe989721ea21a1a86b264
Mohammed Zubair <mohammedzubair10@yahoo.com>
2022-02-02 15:03:36 -0500

    commit tree dc12b34407c557f71f685fcc737adeed2fd5f527

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,24 +1,45 @@
-import React from "react";
+import React,{useState} from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
-      <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
-  );
-}
+  function ShoppingList({ items }) {
 
-export default ShoppingList;
+    const [selectedCategory, setSelectedCategory] = useState("")
+  
+  
+    const handleFilterCategory = (e) =>{
+      setSelectedCategory(e.target.value);
+  
+    }
+
+    const searchResults = items.filter((item) => {
+     return ( selectedCategory === "ALL" ? true : item.category.includes(selectedCategory))
+    })
+  
+ 
+    return (
+      <div className="ShoppingList">
+        <div className="Filter">
+          <select name="filter" onChange={handleFilterCategory} > 
+            <option value="All">Filter by category</option>
+            <option value="Produce">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Dessert">Dessert</option>
+          </select>
+        </div> 
+        <ul className="Items">
+          {searchResults.map((item) => (
+            <Item key={item.id} name={item.name} category={item.category} />
+          ))}
+              
+        
+        </ul>
+      </div>
+    );
+  }
+  
+  
+  
+  
+  
+  
+  export default ShoppingList;
